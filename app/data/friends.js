f391bb63cacabbc2f80789c340101892a2aef047
msheffield <msheffield@adaptivebiotech.com>
2019-11-25 16:49:33 -0800

    modular bbrooo

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         name: "Abed",
-        photo: "",
+        photo: "https://vignette.wikia.nocookie.net/community-sitcom/images/7/7e/Abedfingerpoint.jpg/revision/latest?cb=20130306082535",
         scores: [
             5,
             1,
@@ -17,7 +17,7 @@ var friends = [
     },
     {
         name: "Britta",
-        photo: "",
+        photo: "http://4.bp.blogspot.com/-wOulGiU1-PA/T49wNlTQxbI/AAAAAAAAAqY/kbzCha6LCs4/s1600/Screen+shot+2012-04-18+at+8.59.28+PM.jpg",
         scores: [
             1,
             1,
@@ -33,7 +33,7 @@ var friends = [
     },
     {
         name: "Troy",
-        photo: "",
+        photo: "https://i.pinimg.com/originals/3d/2f/b4/3d2fb482030770cd924325b501fd04c0.png",
         scores: [
             5,
             5,
